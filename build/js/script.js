71d37c0c2bea450cd9c64e129265e657e7555a16
Metalicua <metalicua@gmail.com>
2019-01-11 20:12:15 +0200

    1
    1

--- build/js/script.js
+++ build/js/script.js
@@ -2,13 +2,18 @@
 
 $(document).ready(function(){
     // Slider
-    $('.js_slider').slick({
+    var $sliderBox = $('.js_slider');
+    var $arrowLeft = $('.js_arrow_left');
+    var $arrowRight = $('.js_arrow_right');
+    var $header = $('.header');
+
+    $($sliderBox).slick({
         dots: true,
         infinite: true,
         autoplay: true,
         autoplaySpeed: 3000,
-        prevArrow: $('.js_arrow_left'),
-        nextArrow: $('.js_arrow_right'),
+        prevArrow: $($arrowLeft),
+        nextArrow: $($arrowRight),
     });
 
 // End Slider
@@ -16,33 +21,49 @@ $(document).ready(function(){
 // Responsive
 
     $(window).on('load resize', function () {
+        var $col_4 = $('col-4');
+        var $col_3 = $('col-3');
+        var $col_6 = $('col-6');
+        var $col_8 = $('col-8');
 
         if ($(window).width() <= '768'){
-            $('.slider__discription').removeClass('col-4').addClass('col-8');
-            $('.js_box').removeClass('col-6');
-            $('.js_box_1').removeClass('col-8');
-            $('.js_box_2').removeClass('col-4');
+            $('.slider__discription').removeClass($col_4).addClass($col_8);
+            $('.js_box').removeClass($col_6);
+            $('.js_box_1').removeClass($col_8);
+            $('.js_box_2').removeClass($col_4);
             $('.js_grid').removeClass('col');
-            $('.js_col').removeClass('col-4');
-            $('.js_footer-link').removeClass('col-4');
-            $('.js_footer-col').removeClass('col-6');
-            $('.js_footer-sm-col').removeClass('col-3');
+            $('.js_col').removeClass($col_4);
+            $('.js_footer-link').removeClass( $col_4);
+            $('.js_footer-col').removeClass($col_6);
+            $('.js_footer-sm-col').removeClass($col_3);
             $('.school__btn').remove();
             $('.school__button').append('<a href="#" class="school__btn btn">learn more</a>')
         }
         if ($(window).width() > '768'){
-            $('.slider__discription').addClass('col-4').removeClass('col-8');
-            $('.js_box').addClass('col-6');
-            $('.js_box_1').addClass('col-8');
-            $('.js_box_2').addClass('col-4');
+            $('.slider__discription').addClass( $col_4).removeClass('col-8');
+            $('.js_box').addClass($col_6);
+            $('.js_box_1').addClass($col_8);
+            $('.js_box_2').addClass( $col_4);
             $('.js_grid').addClass('col');
-            $('.js_col').addClass('col-4');
-            $('.js_footer-link').addClass('col-4');
-            $('.js_footer-col').addClass('col-6');
-            $('.js_footer-sm-col').addClass('col-3');
+            $('.js_col').addClass( $col_4);
+            $('.js_footer-link').addClass( $col_4);
+            $('.js_footer-col').addClass($col_6);
+            $('.js_footer-sm-col').addClass($col_3);
             $('.school__btn').remove();
             $('.school__button').append('<a href="#" class="school__btn btn">Learn More About The Wilton School</a>')
+
+            $(window).scroll(function(){
+        
+                if ( $(this).scrollTop() > 0 ) {
+                    $($header).addClass('fixed');
+                    $('.promo__container').css({'margin-top': '175px'});
+                } else {
+                    $($header).removeClass('fixed');
+                    $('.promo__container').css({'margin-top': '0'});
+                }
+            });
         }
+
 
     });
 
@@ -67,13 +88,16 @@ $(document).ready(function(){
     $('.js_footer_btn').on('click', function(){
        
         $('html, body')
-            .animate({scrollTop:$('.header')
+            .animate({scrollTop:$($header)
             .position().top}, 3000);
     });
 
     $('.footer__menu').on('click', function() {
         $('.js_footer_mob-box').toggleClass('show');
     });
+
+    
+      
 
 
 });
